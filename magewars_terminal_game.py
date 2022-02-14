c4876e88baa3de6ee25264ed0e016092baa56a0e
Taylor <ta_mcausland@hotmail.com>
2022-02-14 12:33:28 -0500

    added __repr__ methods, added 'school' to class variables, created test area. Everything working as expected to this point

--- magewars_terminal_game.py
+++ magewars_terminal_game.py
@@ -2,8 +2,9 @@ import random
 
 #defines class for unit that is capable of taking actions.  constructor initializes stats and conditions
 class Unit:
-    def __init__(self, name, max_health, weapon, attack_dice, armor=0):
+    def __init__(self, name, school, max_health, weapon, attack_dice, armor=0, is_ranged=False):
         self.name = name
+        self.school = school
         self.max_health = max_health
         self.health = max_health
         self.weapon = weapon
@@ -11,6 +12,7 @@ class Unit:
         self.full_armor = armor
         self.armor = armor
         self.is_active = False
+        self.is_ranged = is_ranged
         self.is_dead = False
         self.is_poison = False
         self.is_burn = False
@@ -49,18 +51,22 @@ class Unit:
         return sum
 
     def attack(self, target):
+        print(f'{self.name} is attacking {target.name}')
         target.lose_health(self.roll_dice())
 
 
 class Mage(Unit):
-    def __init__(self, name, max_health, start_mana, mana_regen, weapon, attack_dice, armor=0):
-        super().__init__(name, max_health, weapon, attack_dice, armor)
+    def __init__(self, name, school, max_health, start_mana, mana_regen, weapon, attack_dice, armor=0, is_ranged=False):
+        super().__init__(name, school, max_health, weapon, attack_dice, armor, is_ranged)
         self.mana = start_mana
         self.mana_regen = mana_regen
         self.spellbook = []
         self.front_line = [self]
         self.graveyard = []
         self.is_active = True
+
+    def __repr__(self):
+        return f"{self.name} is a {self.school} Mage.  They have {self.health} health and attack with {self.weapon} using d{self.attack_dice} dice.  They have {self.mana} mana and regain {self.mana_regen} at the start of every round."
 
     def gain_mana(self, value):
         self.mana += value
@@ -86,17 +92,24 @@ class Mage(Unit):
 
     
 class Creature(Unit):
-    def __init__(self, name, max_health, mana_cost, weapon, attack_dice, armor=0):
-        super().__init__(name, max_health, weapon, attack_dice, armor)
+    def __init__(self, name, school, max_health, mana_cost, weapon, attack_dice, armor=0, is_ranged=False):
+        super().__init__(name, school, max_health, weapon, attack_dice, armor, is_ranged)
         self.mana_cost = mana_cost
+
+    def __repr__(self):
+        return f"{self.name} is a {self.school} creature.  It has {self.health} health and attacks with {self.weapon} using d{self.attack_dice} dice."
 
 
 class Spell:
-    def __init__(self, name, action, attack_dice, mana_cost):
+    def __init__(self, name, school, action, attack_dice, mana_cost):
         self.name = name
+        self.school = school
         self.action = action
         self.attack_dice = attack_dice
         self.mana_cost = mana_cost
+
+    def __repr__(self):
+        return f"{self.name} is a {self.school} spell that costs {self.mana_cost} mana.  Its target will {str(self.action)} using d{self.attack_dice} dice."
     
     def roll_dice(self):
         sum = 0
@@ -104,3 +117,32 @@ class Spell:
             sum += random.randint(1, die)
         print(f'Your spell dice rolled {sum}')
         return sum
+
+
+
+
+########TEST AREA#########
+dragon1 = Creature("Alfiya", "Holy", 19, 24, "holy dragon breath", [8, 8, 6], 3)
+cleric1 = Creature("Artemis", "Holy", 10, 14, "a spear", [6, 6], 1, True)
+angel1 = Creature("Cassiel", "Holy", 8, 9, 'holy magic', [4, 4])
+angel2 = Creature("Guardian Angel", "Holy", 12, 12, 'a shortsword', [8, 8], 1)
+
+
+#angel2.attack(dragon1)
+#angel1.attack(dragon1)
+#dragon1.gain_health(dragon1.roll_dice())
+
+player1 = Mage("Asyra", "Holy", 34, 9, 9, "the Staff of Asyra", [6, 6])
+player1.spellbook = [dragon1, cleric1, angel1, angel2]
+#player1.list_spellbook()
+#player1.list_front_line()
+
+player1.list_spellbook()
+print('-------')
+player1.list_graveyard()
+print('\n-------\n-------\n')
+player1.spellbook.remove(dragon1)
+player1.graveyard.append(dragon1)
+player1.list_graveyard()
+print('----------')
+player1.list_spellbook()
